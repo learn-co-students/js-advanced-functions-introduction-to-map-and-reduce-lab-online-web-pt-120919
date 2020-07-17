a9944ad7884415f39035f7f2057258ba07505ccb
Robert Shields <robert.shieldsiv@gmail.com>
2020-07-16 21:39:07 -0400

    Done.

--- index.js
+++ index.js
@@ -33,3 +33,25 @@ let mapToSquare = function(sourceArray) {
         return newArray
     
 }
+
+function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < src.length; i++ ) {
+      total = total + src[i]
+    }
+    return total
+  }
+  
+  function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
